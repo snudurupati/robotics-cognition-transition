df11f3ff96a389901a4b13ad9cb1a65aef7cb44c
sreeram <snudurupati@outlook.com>
2025-11-07 06:54:40 -0800

    Added a few basic starter CPP programs.

---
+++ cpp-foundations/day0/NumberSquared.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+double square(double x)
+{
+    return x*x;
+}
+
+int main() 
+{
+    cout << "the square of " << "5.5 is: " << square(5.5) << "\n";
+}
