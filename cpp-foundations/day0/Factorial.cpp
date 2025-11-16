30b24f34827af2ddc35e807d69d8754c2ad446b4
sreeram <snudurupati@outlook.com>
2025-11-15 19:11:59 -0800

    worked with functions and loops.

---
+++ cpp-foundations/day0/Factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int factorial(int n)
+{
+    for (int i=n; i > 1; i--)
+    {
+        std::cout << i <<":"<< n << "\n";
+        n = n*(i-1);
+    }
+
+    return n;
+
+}
+
+int main()
+{
+    int x {10};
+    std::cout<< "factorial of " << x <<" is: " << factorial(x) << "\n";
+}
