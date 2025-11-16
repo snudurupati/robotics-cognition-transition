b303f81e8dacd73ed460b54968bd44eeea539765
sreeram <snudurupati@outlook.com>
2025-11-15 19:28:32 -0800

    add function forward decleration.

--- cpp-foundations/day0/Factorial.cpp
+++ cpp-foundations/day0/Factorial.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 
-int factorial(int n)
-{
-    for (int i=n; i > 1; i--)
-    {
-        std::cout << i <<":"<< n << "\n";
-        n = n*(i-1);
-    }
-
-    return n;
-
-}
+int factorial(int); //forward decleration
 
 int main()
 {
     int x {10};
     std::cout<< "factorial of " << x <<" is: " << factorial(x) << "\n";
+}
+
+int factorial(int n)
+{
+    for (int i=n; i > 1; i--)
+    {
+        //std::cout << i <<":"<< n << "\n";
+        n = n*(i-1);
+    }
+
+    return n;
+
 }
